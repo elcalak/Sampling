55505834809f441a83d6e3bfa600af87a9737cc0
elcalak <ing.christophergl@gmail.com>
2025-04-15 16:42:38 -0600

    Logic errors and Linux support

--- AudioControls.py
+++ AudioControls.py
@@ -18,32 +18,32 @@ Compile in environment (in Arch): Environment/piplibs/bin/python
 
 """
 
-import pygame as pg
-from time import sleep
+import pygame as pg #Call the module pygame as pg
+from time import sleep #From module time import sleep function
 
-def play(file): #Function play for mp3 files
+def load(file): #Function load files
+    
+    global sound #Global variable sound to save the current audio
+    print("Im load fun!") #Line for work check
 
     try: #Init try
         
-        print("Hi im a try!")
+        print("Hi im load try!") #Line for work check
 
         pg.mixer.init() #Start module mixer from pygame
-        global sound
-        print("Hey im initi the mixer!") #Line for work check
+        print("Hey im starting the mixer!") #Line for work check
 
         if file.endswith(".mp3") or file.endswith(".ogg"): #Start condicional for compress format
             
             sound = pg.mixer.music.load(file) #Load MP3
-            sound.mixer.music.play() #Start play
-            print("Im playmp3 fun!") #Line for work check
+            print("Im loadmp3 fun!") #Line for work check
             return sound, True #Return pygame mixer
         #Finish condicional for compress formats
 
         elif file.endswith(".wav"): #Start condicional for wav format
             
             sound = pg.mixer.Sound(file) #Load WAV
-            sound.play() #Start play
-            print("Im playwav fun!") #Line for work check
+            print("Im loadwav fun!") #Line for work check
             return sound, True #Return pygame mixer
         #Finish condicional for wav format
 
@@ -56,17 +56,82 @@ def play(file): #Function play for mp3 files
     
     except pygame.error as e: #Init except
     
-        print(f"¡Oops! Ocurrió un error al cargar o reproducir el archivo: {file}") #Print error message
-        print(f"Error de Pygame: {e}") #Print error message
+        print(f"Error to load or play the file: {file}") #Print error message
+        print(f"Pygame error: {e}") #Print error message
         return None, False #Return none and false
     #Finish Except
 
     except FileNotFoundError: #Init except
         
-        print(f"¡Oops! No encontré el archivo: {file}") #Print error message
+        print(f"File: {file} not found") #Print error message
+        return None, False #Return none and false
+    #Finish Except
+
+def play(): #Function play
+
+    print("Im play fun!") #Line for work check
+
+    try: #Init try
+        
+        print("Hi im play try!") #Line for work check
+
+        if sound == None: #Start condicional for compress format
+            
+            sound.mixer.music.play() #Start play
+            print("Im playmp3 fun!") #Line for work check
+            return sound, True #Return pygame mixer
+        #Finish condicional for compress formats
+
+        else: #Start condicional for wav format
+            
+            sound.play() #Start play
+            print("Im playwav fun!") #Line for work check
+            return sound, True #Return pygame mixer
+        #Finish condicional for wav format
+       
+    #Finish Try 
+    
+    except pygame.error as e: #Init except
+    
+        print(f"Error to load or play the file: {file}") #Print error message
+        print(f"Pygame error: {e}") #Print error message
+        return None, False #Return none and false
+    #Finish Except
+
+def stop():
+    
+    print("Im stop fun!") #Line for work check
+
+    try: #Init try
+        
+        print("Hi im a try!") #Line for work check
+
+        if sound == None: #Start condicional for compress format
+            
+            sound.mixer.music.stop() #Stop play
+            print("Im stopmp3 fun!") #Line for work check
+            return sound, True #Return pygame mixer
+        #Finish condicional for compress formats
+
+        else: #Start condicional for wav format
+
+            sound.stop() #Stop play
+            print("Im stopwav fun!") #Line for work check
+            return sound, True #Return pygame mixer
+        #Finish condicional for wav format
+
+    #Finish Try 
+    
+    except pygame.error as e: #Init except
+    
+        print(f"Error to load or play the file: {file}") #Print error message
+        print(f"Pygame error: {e}") #Print error message
         return None, False #Return none and false
     #Finish Except
 
 print("Im alavie")
-file = '/home/elcalak/Downloads/Angustia.wav'
-play(file)
+archivo = '/home/elcalak/Downloads/Angustia.wav'
+load(archivo)
+play()
+sleep(5)
+stop()
