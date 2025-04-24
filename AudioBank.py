45bb99a983be055e0a343a844f37ff2cc724538e
elcalak <ing.christophergl@gmail.com>
2025-04-23 22:46:25 -0600

    Add Audio Bank creation

--- AudioBank.py
+++ AudioBank.py
@@ -18,19 +18,51 @@ Compile in environment (in Arch): Environment/piplibs/bin/python
 
 """
 
-class AudioBank:
+import os #Call the module os to control path files 
+import tkinter as tk
+from tkinter import filedialog
 
-    DEFAULT_BANK_PATH = "/home/elcalak/Repositorys/elcalak/Sampling/AudioBanks/Default"
+class AudioBank: #Start class Audio bank
 
-    def __init__(self, name="DefaultBank"):
+    DEFAULT_BANK_PATH = "/home/elcalak/Repositorys/elcalak/Sampling/AudioBanks/Default" #Set default path for default bank
 
-        self.name = name
-        self.base_path = AudioBank.DEFAULT_BANK_PATH
+    def __init__(self, name="DefaultBank"): #Init contructor to default bank
 
-        self._sounds = {}
+        self.name = name #Set the name "DefaultBank"
+        self.base_path = AudioBank.DEFAULT_BANK_PATH #Set default bank path
 
-        for i in range(1,10):
+        self._sounds = {} #Create a sound library 
+
+        for i in range(1,10): #Init for to load the defualt audio files
             
-            placeholder_path = os.path.join(self.base_path, f"slot{i}.wav")
-            self._sounds[i] = placeholdername
+            placeholder_path = os.path.join(self.base_path, f"slot{i}.wav") #Search the files in deafult path
+            print(f"Hi im slot{i}") #Work check
+            self._sounds[i] = placeholder_path #Saves files in the sound library
+        #End for
+    
+    #End constructor
 
+    def CreateBak(): #Function to create a new bank
+
+        root = tk.Tk() #Create a Tk object
+        root.withdraw() #Hide the main dialog
+
+        BankName = input("Select bank name: ") #Ask user to name for bank
+
+        Bank.name = BankName #Set the name assigned
+        Bank.base_path = filedialog.askdirectory(title = "Select folder",) #Set the base path from bank
+
+        tk.Tk().destroy() #Close root dialog
+
+        if Bank.base_path: #Verify slection
+            
+            print(f"Folder select: {Bank.base_path}") #Print Succes path select
+
+        else: #Contradiction
+
+            print("No folder select.") #Print error message
+        
+        for i in range(1,10): #Init for to load the audio files
+
+            Bank.sounds[i] = filedialog.askopenfile(title = f"Select slot{i}") #Select audio files to slot
+            print(f"Hi im slot{i}") #Work check
